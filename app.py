f1058e4b487b30db70ad541528241eb7eaa267f7
Priyanshu Agarwal <priyanshuone6@gmail.com>
2023-10-29 00:53:38 +0530

    Improve app

--- app.py
+++ app.py
@@ -18,13 +18,16 @@ def get_user_input(header_type):
 
 # Streamlit App
 st.title("WhatsApp Bulk Message Sender")
+
 st.markdown(
     """<style>[class="st-emotion-cache-76z9jo e116k4er2"]{display: none;}""",
     unsafe_allow_html=True,
 )
+
 # Input fields
 all_templates = get_message_templates()
 template_name = st.selectbox("Select Template Name", all_templates)
+
 # Header selection
 header_type = st.radio("Select Template Header Type", ["Text", "Image"])
 
@@ -34,15 +37,11 @@ country_code = st.number_input("Enter Country Code", value=91)
 phone_number = st.number_input("Enter Phone Number", value=0)
 language = st.selectbox("Select Language", ["Hindi", "English"])
 
-
 # Variables dropdown menu
-num_variables = st.selectbox("Select Number of Variables", [1, 2, 3, 4, 5])
+num_variables = st.selectbox("Select Number of Variables", list(range(1, 11)))
 
 # Gather user inputs for variables
-variables = []
-for i in range(num_variables):
-    variable_name = st.text_input("Variable {{" + f"{i + 1}" + "}}")
-    variables.append(variable_name)
+variables = [st.text_input(f"Variable {{ {i + 1} }}") for i in range(num_variables)]
 
 # Display the selected inputs in a nice box
 st.markdown("### Selected Inputs")
@@ -52,28 +51,28 @@ selected_inputs = f"""
 - **Country Code:** {country_code}
 - **Phone Number:** {phone_number}
 - **Language:** {language}
-
+- **Number of Variables:** {num_variables}
 """
-selected_inputs += f"- **Number of Variables:** {num_variables}\n"
-for i in range(num_variables):
-    selected_inputs += "- **Variable {{" + f"{i + 1}" + "}}:** " + f"{variables[i]}\n"
+selected_inputs += "\n".join(
+    [f"- **Variable {{ {i + 1} }}:** {variables[i]}" for i in range(num_variables)]
+)
 st.info(selected_inputs)
 
 # Submit button
 if st.button("Send Message"):
-
+    image_component = None
     if user_inputs is not None:
         stdout = upload_media(user_inputs.read(), user_inputs.type)
         image_id = json.loads(stdout)["id"]
-
         image_component = {
             "type": "header",
             "parameters": [{"type": "image", "image": {"id": image_id}}],
         }
+
     component = generate_components(variables)
-    if user_inputs is not None:
+    if image_component:
         component.insert(0, image_component)
-    # st.write(template_name, language, country_code, phone_number, component)
+
     response = send_whatsapp_message(
         template_name=template_name,
         language=language,
@@ -82,7 +81,4 @@ if st.button("Send Message"):
         components=component,
     )
     data = json.loads(response["response"])
-    if data["messages"][0]["message_status"] == "accepted":
-        st.success(response)
-    else:
-        st.error(response)
+    st.write(data)
